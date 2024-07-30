528d5137069660cfbfb72a0c71d3bee28348dd9c
mclacore <mclacore@gmail.com>
2024-07-30 13:41:59 -0700

    update formatters, add custom bicep formatter

--- init.lua
+++ init.lua
@@ -690,13 +690,32 @@ require('lazy').setup({
         }
       end,
       formatters_by_ft = {
+        bash = { 'beautysh' },
+        bicep = { 'custom_bicep_fmt' },
+        go = { 'goimports', 'gofumpt', 'gotests' },
+        hcl = { 'hclfmt' },
+        html = { 'prettierd', 'prettier', 'html-beautifier' },
+        javascript = { 'prettierd', 'prettier' },
+        javascriptreact = { 'prettierd', 'prettier' },
+        jq = { 'jq' },
+        json = { 'prettierd', 'prettier', 'jq', 'jsonnetfmt', 'fixjson' },
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        markdown = { 'prettierd', 'prettier', 'markdownlint', 'mdformat' },
+        python = { 'isort', 'black' },
+        ruby = { 'rubocop' },
+        terraform = { 'tofu_fmt', 'terraform_fmt' },
+        toml = { 'taplo' },
+        typescript = { 'prettierd', 'prettier', 'ts-standard' },
+        typescriptreact = { 'prettierd', 'prettier' },
+        yaml = { 'prettierd', 'prettier', 'yamlfix', 'yamlfmt' },
+      },
+      formatters = {
+        custom_bicep_fmt = {
+          command = 'az', --az bicep format --file template.bicep
+          args = { 'bicep', 'format', '--file', '$FILENAME' },
+          stdin = false,
+          timeout = 5000,
+        },
       },
     },
   },
@@ -730,12 +749,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
